387aabf7f6d5238019df0a88ecb898f4962858d4
Tuấn Anh Hoàng <108229424+tuanballboo235@users.noreply.github.com>
2025-06-07 21:19:52 +0700

    remove netflix controller, add orderController

---
+++ TAPrim/API/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TAPrim.Application.Services;
+
+namespace TAPrim.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class OrderController : ControllerBase
+	{
+		private readonly IOrderService _orderService;
+		OrderController(IOrderService orderService) => _orderService = orderService;
+		
+		[HttpGet("get-order-by-product-account-id")]
+		public IActionResult GetOrderByProductAccountId()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ TAPrim/Application/Services/ServiceImpl/OrderService.cs
@@ -0,0 +1,6 @@
+﻿namespace TAPrim.Application.Services.ServiceImpl
+{
+	public class OrderService:IOrderService
+	{
+	}
+}
