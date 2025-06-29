c95f05fc1013db3d9d95533d9dd7bae5ce683bc5
Tuấn Anh Hoàng <108229424+tuanballboo235@users.noreply.github.com>
2025-06-29 22:55:06 +0700

    Add coupon feature

---
+++ TAPrim/Application/Services/ServiceImpl/CouponService.cs
@@ -0,0 +1,48 @@
+﻿using TAPrim.Application.DTOs.Common;
+using TAPrim.Common.Helpers;
+using TAPrim.Infrastructure.Repositories;
+using TAPrim.Models;
+using TAPrim.Shared.Constants;
+
+namespace TAPrim.Application.Services.ServiceImpl
+{
+	public class CouponService : ICouponService
+	{
+		private readonly ICouponRepository _couponRepository;
+		public CouponService(ICouponRepository couponRepository)
+		{
+			_couponRepository = couponRepository;
+		}
+
+		public async Task<ApiResponseModel<object>> GetCouponByCouponCode(string couponCode)
+		{
+			try
+			{
+				var coupon = await _couponRepository.FindByCode(couponCode);
+				if (coupon == null) {
+					return new ApiResponseModel<object>
+					{
+						Status = ApiResponseStatusConstant.FailedStatus,
+						Message = "Mã giảm giá không khả dụng",
+						Data = await _couponRepository.FindByCode(couponCode),
+					};
+				}
+				return new ApiResponseModel<object>
+				{
+					Status = ApiResponseStatusConstant.SuccessStatus,
+					Message = "Mã giảm giá khả dụng",
+					Data = await _couponRepository.FindByCode(couponCode),
+				};
+			}
+			catch (Exception ex)
+			{
+				return new ApiResponseModel<object>
+				{
+					Status = ApiResponseStatusConstant.FailedStatus,
+					Message = ex.Message,
+				};
+
+			}
+		}
+	}
+}
--- TAPrim/Infrastructure/Repositories/ICouponRepository.cs
+++ TAPrim/Infrastructure/Repositories/ICouponRepository.cs
@@ -5,6 +5,6 @@ namespace TAPrim.Infrastructure.Repositories
     public interface ICouponRepository
 	{
 		Task<Coupon?> FindById(int? couponId);
-
+		Task<Coupon?> FindByCode(string couponCode);
 	}
 }
